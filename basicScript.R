80d8d6a5529a1a29f445082a2e183f52f7416408
KaiyoteSoft <Spikeyhedgehog141@gmail.com>
2020-10-05 15:42:33 -0700

    Added new script that reads in a .xlsx data

---
+++ basicScript.R
@@ -0,0 +1,11 @@
+##### EEMB 144L Intro to R with CAL FIRE data ########
+#Kai Oda
+#10/5/2020 
+
+# Load the tidyverse package
+library(tidyverse)
+library(readxl)
+
+## Load the sample fire dataset 
+
+
